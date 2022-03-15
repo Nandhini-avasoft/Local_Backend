9fc0fdaab0a15d9b7e4f1b2b6bb05dc691afa245
Nandhini-avasoft <97678199+Nandhini-avasoft@users.noreply.github.com>
2022-03-15 19:02:05 +0530

    Add files via upload

---
+++ Local_Backend/Local_Backend/Services/CarouselService/ICarouselService.cs
@@ -0,0 +1,14 @@
+﻿using Local_Backend.BOs;
+using Local_Backend.Helpers;
+using System.Collections.ObjectModel;
+
+namespace Local_Backend.Services.CarouselService
+{
+    public interface ICarouselService
+    {
+        public ServiceResults<int> AddCarouselData(CarouselBo carousel);
+        public ServiceResults<ObservableCollection<CarouselBo>> FetchConditionalCarouselData(string title,string descript);
+        public ServiceResults<int> UpdateCarouselData(string title);
+        public ServiceResults<int> DeleteCarouselData(int carouselId);
+    }
+}
